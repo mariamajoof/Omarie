80e68fa57def37de762b5838ff34d105514ad9b2
Mariama <joofwais@googlemail.com>
2022-06-07 15:57:03 +0100

    Add functionally

--- src/main/java/com/tsi/mariama/joof/program/Actor.java
+++ src/main/java/com/tsi/mariama/joof/program/Actor.java
@@ -1,7 +1,5 @@
 package com.tsi.mariama.joof.program;
 
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.*;
 @Entity
 @Table(name="actor")
@@ -10,27 +8,39 @@ public class Actor {
 
     //Attributes
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int actorID;
-    String firstName;
-    String lastName;
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    int actor_id;
+    String first_name;
+    String last_name;
     //Constructors
-    public Actor(String firstName, String lastName){
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Actor(String first_name, String last_name){
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
 
     public Actor(){}
 
     public int getActorID() {
-        return actorID;
+        return actor_id;
+    }
+
+    public void setActor_id(int actor_id) {
+        this.actor_id = actor_id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
 
     public String getFirstName() {
-        return firstName;
+        return first_name;
     }
 
     public String getLastName() {
-        return lastName;
+        return last_name;
     }
 }
--- src/main/java/com/tsi/mariama/joof/program/MyFirstMicroServiceApplication.java
+++ src/main/java/com/tsi/mariama/joof/program/MyFirstMicroServiceApplication.java
@@ -3,23 +3,52 @@ package com.tsi.mariama.joof.program;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @CrossOrigin(origins = "*") // needed for receiving request via api
 @SpringBootApplication
 @RestController // Handles GET, POST, DELETE, PUT requests
 @RequestMapping("/home")//base url
+
+
+
 public class MyFirstMicroServiceApplication {
-@Autowired
-private ActorRepository actorRepository;
+	@Autowired
+	private ActorRepository actorRepository;
+
+	private String saved = "Saved";
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstMicroServiceApplication.class, args);
 	}
-public MyFirstMicroServiceApplication(ActorRepository actorRepository){
-		this.actorRepository = actorRepository;
-}
+	public MyFirstMicroServiceApplication(ActorRepository actorRepository){
+			this.actorRepository = actorRepository;
+	}
+
+	//Get data from actor table
+	@GetMapping("/allActors")
+	public @ResponseBody
+	Iterable<Actor> getAllActors() {
+		return actorRepository.findAll();
+	}
+
+	// add data to the table
+	@PostMapping("/addActor")
+	public @ResponseBody String addActor(@RequestParam String first_name, @RequestParam String last_name ){
+
+		Actor addActor = new Actor(first_name,last_name);
+		actorRepository.save(addActor);
+		return saved;
+	}
+
+	//update data
+	//@PutMapping("/updateActor")
+	//public @ResponseBody String updateActor(@RequestParam String firstName, String lastName ){
+		//Actor updateActor = new Actor()
+
+	//}
+
+
+	//delete
 
 }
